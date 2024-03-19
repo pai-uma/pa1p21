1970e53e5154775c36ea39491141911f4f9a62f2
Ernesto Pimentel <pi@ernestopimentel.es>
2024-03-19 08:45:08 +0100

    Inclusión de comentarios con libros a añadir en PruebaLibreriaOfertaFlex.java

--- src/main/java/PruebaLibreriaOfertaFlex.java
+++ src/main/java/PruebaLibreriaOfertaFlex.java
@@ -1,6 +1,18 @@
 
 public class PruebaLibreriaOfertaFlex {
-
+	/*
+	("george orwell", "1984", 8.20)
+	("Philip K. Dick", "¿Sueñan los androides con ovejas eléctricas?", 3.50)
+	("Isaac Asimov", "Fundación e Imperio", 9.40)
+	("Ray Bradbury", "Fahrenheit 451", 7.40)
+	("Aldous Huxley", "Un Mundo Feliz", 6.50)
+	("Isaac Asimov", "La Fundación", 7.30)
+	("William Gibson", "Neuromante", 8.30)
+	("Isaac Asimov", "Segunda Fundación", 8.10)
+	("Isaac Newton", "arithmetica universalis", 7.50)
+	("George Orwell", "1984", 6.20)
+	("Isaac Newton", "Arithmetica Universalis", 10.50)
+	*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	}
